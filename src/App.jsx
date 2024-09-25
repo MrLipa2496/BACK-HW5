c70c09bf59053297960ee24333d0fe4179fc9aca
MrLipa2496 <sasalipa216@gmail.com>
2024-09-25 16:26:31 +0300

    Add structure BasePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import BasePage from './pages/BasePage'
 
 function App () {
-  return <div>App</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<BasePage />}>
+          <Route></Route>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
