38c7bdd8f59c6271bee29f02e00e0aae650068d0
MrLipa2496 <sasalipa216@gmail.com>
2024-10-18 18:54:35 +0300

    Add middleware errorHandlers

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { errorHandlers } = require('./middleware');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
 
 module.exports = app;
